b561413402559a431b517d1fbb84d90e716577dd
Grzegorz Łucki <glucki@teonite.com>
2020-03-06 01:07:49 +0100

    Add clear cart data functionality

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from core.models import Diamond
+from core.models import Diamond, Cart
 
 
+def clear_cart(modeladmin, request, queryset):
+    queryset.update(data=[])
+
+
+clear_cart.short_description = "Clear Cart data"
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['data', 'updated']
+    ordering = ['updated']
+    actions = [clear_cart]
+
+
+admin.site.register(Cart, CartAdmin)
 admin.site.register(Diamond)
--- backend/core/utils.py
+++ backend/core/utils.py
@@ -8,9 +8,10 @@ def send_email(email_address):
 
     if len(cart.data) > 0:
         totals = [str(diamond["quantity"]) + "x " + diamond["type"] for diamond in cart.data]
-        body = "You have " + ", ".join(totals) + " in your cart"
+        amount = sum([diamond["quantity"] for diamond in cart.data])
+        body = "You have " + ", ".join(totals) + " in your cart.\nTotal amount of diamonds: " + str(amount) + "."
     else:
-        body = 'Your shopping cart is empty'
+        body = 'Your shopping cart is empty.'
 
     email = EmailMessage('Diamonds in your cart', body, to=[email_address])
     email.send()
